a861db98ae28ff6a314ef3ae22d8ad27de25dd29
unknown <muhamed.kapo@2gimnazija.edu.ba>
2023-04-19 23:28:49 +0200

    dodani prototipi 3 kontrolera i 3 rute

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('subject-classes', 'SubjectClassController');
+
+Route::resource('school-classes', 'SchoolClassController');
